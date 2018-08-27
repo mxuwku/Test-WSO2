b0113dccc2055e9fb650895923eb8d975eea4425
EYUZ78 <ela.yudhanira@scania.com>
2018-08-27 13:55:35 +0200

    Fix left menu problems - Left menu stays in the correct order when active - Added active menu styling to sublevel menu

--- repository/deployment/server/jaggeryapps/store/site/themes/wso2/js/left-navigation.js
+++ repository/deployment/server/jaggeryapps/store/site/themes/wso2/js/left-navigation.js
@@ -7,9 +7,9 @@ $(window).load(function(){
         e.preventDefault();
 
 
-        if(animating){
-            return;
-        }
+        // if(animating){
+        //     return;
+        // }
 
         var delay = 0,
 	        el = $(this),
@@ -42,44 +42,62 @@ $(window).load(function(){
             return;
         };
 
-        animating = true;
+        animating = false;
         $(el).find('.left-menu-item').css('background-color',color);
-        $.when(
-            el.delay(delay).animate({
-                top: -distance + 'px'
-            },{
-                duration:0,
-                step:function(now,fx){
-                    if(now == 0){
-                        fx.end = -($(fx.elem).offset().top  - $(".navigation").offset().top);
-                    }
-                }
-            })
-        ).done(function () {
-            $(".navigation ul li.active").removeClass('active');
-            el.insertBefore(prev).css('top','0px').addClass('active');
-            el.find('.left-menu-item').removeAttr('style');
-            animating = false;
+        $(".navigation ul li.active").removeClass('active');
+        //el.insertBefore(prev).css('top','0px').addClass('active');
+        el.addClass('active');
+        el.find('.left-menu-item').removeAttr('style');
+        animating = false;
 
-            if(hasSubMenu){
-                el.find('.sublevel-menu').slideDown(function(){
-                    el.find('.left-menu-item i').removeClass('fw-down').addClass('fw-up');
-                });
-            }
+        if(hasSubMenu){
+            el.find('.sublevel-menu').slideDown(function(){
+                el.find('.left-menu-item i').removeClass('fw-down').addClass('fw-up');
+            });
+        }
 
-            el.is(':hidden') ? el.show():'';
-
-            setTimeout(function(){
-                location.replace(url);
-            },500)
-
-        });
+        el.is(':hidden') ? el.show():'';
+        
+        setTimeout(function(){
+            location.replace(url);
+        },500)
+        // $.when(
+        //     el.delay(delay).animate({
+        //         top: -distance + 'px'
+        //     },{
+        //         duration:0,
+        //         step:function(now,fx){
+        //             if(now == 0){
+        //                 fx.end = -($(fx.elem).offset().top  - $(".navigation").offset().top);
+        //             }
+        //         }
+        //     })
+        // ).done(function () {
+        //     $(".navigation ul li.active").removeClass('active');
+        //     //el.insertBefore(prev).css('top','0px').addClass('active');
+        //     el.addClass('active');
+        //     el.find('.left-menu-item').removeAttr('style');
+        //     animating = false;
+        //
+        //     if(hasSubMenu){
+        //         el.find('.sublevel-menu').slideDown(function(){
+        //             el.find('.left-menu-item i').removeClass('fw-down').addClass('fw-up');
+        //         });
+        //     }
+        //
+        //     el.is(':hidden') ? el.show():'';
+        //
+        //     setTimeout(function(){
+        //         location.replace(url);
+        //     },500)
+        //
+        // });
     }).children('.sublevel-menu').find('li').click(function(e){
         e.stopImmediatePropagation();
     });
 
-    if(!$('.navigation ul li.active').is(':first-child')){
-        !$('.navigation ul li.active').trigger('click');
-    }
-    
+    // if(!$('.navigation ul li.active').is(':first-child')){
+    //     !$('.navigation ul li.active').trigger('click');
+    // }
+
 });
